712c11cfa92e6ed4f7a59cca8e33555b89b42e58
Saiboth <35424639+Saiboth@users.noreply.github.com>
2021-12-03 17:19:32 +0100

    Update Day_3.py
    Fix dubbele paste part1

--- Day_3.py
+++ Day_3.py
@@ -2,8 +2,6 @@ def part1(inp):
     inp = inp.split()
     gamma = ""
     epsilon = ""
-    g0, g1 = [0,0], [0,0]
-    e0, e1 = [0,0], [0,0]
     t = [''.join(s) for s in zip(*inp)]
     for g in t:
         if g.count("1") > g.count("0"):
@@ -12,31 +10,15 @@ def part1(inp):
             gamma += "0"
     epsilon = int(''.join(['1' if i == '0' else '0' for i in gamma]), 2)
     gamma = int(gamma, 2)
-    return(gamma, epsilon, gamma*epsilon)
-        
-part1(input2)def part1(inp):
-    inp = inp.split()
-    gamma = ""
-    epsilon = ""
-    g0, g1 = [0,0], [0,0]
-    e0, e1 = [0,0], [0,0]
-    t = [''.join(s) for s in zip(*inp)]
-    for g in t:
-        if g.count("1") > g.count("0"):
-            gamma += "1"
-        else:
-            gamma += "0"
-    epsilon = int(''.join(['1' if i == '0' else '0' for i in gamma]), 2)
-    gamma = int(gamma, 2)
-    return(gamma, epsilon, gamma*epsilon)
-        
+    return(gamma, epsilon, gamma * epsilon)
+
 part1(input2)
 
 def part2(inp):
     inp = inp.split()
     oxygen = int(filtr(inp, 0, 1), 2)
     scrubber = int(filtr(inp, 0, 0), 2)
-    return(oxygen, scrubber, oxygen*scrubber)
+    return(oxygen, scrubber, oxygen * scrubber)
         
 def filtr(l, p, x):
     if len(l) == 1:
@@ -47,7 +29,6 @@ def filtr(l, p, x):
     for line in l:
         if line[p] == str(x):
             a += 1
-            
         else:
             b += 1
     if x == 0:
@@ -65,6 +46,5 @@ def filtr(l, p, x):
                 new_l.append(line)
     p += 1
     return filtr(new_l, p, x)
-        
-    
+
 part2(input2)
